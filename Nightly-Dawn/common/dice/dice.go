ea87a7775e418bd0346e5638b113bde9ca621380
Shawn Robertson <SLERobertson@gmail.com>
2012-04-12 16:13:40 -0700

    Updating dice to use a struct, can roll multiple now

--- Nightly-Dawn/common/dice/dice.go
+++ Nightly-Dawn/common/dice/dice.go
@@ -7,30 +7,46 @@ import (
 	"time"
 )
 
+// Represents a single die roll (with explosions)
+// NOTE: Should the struct be modified in order to mention if it has an explosion?
+// This can be figured out if list has more than one element.
+type DieRoll struct {
+	Total uint16
+	List  []uint16
+}
+
 // Creates a closure with a seeded generator for a specific die type
-func DieType (NumSides int) func() (sum int, rolls []int) {
+func DieType (NumSides int) func() (res *DieRoll) {
 	generator := rand.New(rand.NewSource(int64(time.Now().Nanosecond())))
 
-	return func () (sum int, rolls []int) {
-		sum = 0
-		roll := generator.Intn(NumSides) + 1
+	return func () (res *DieRoll) {
+		res       = new(DieRoll)
+		res.Total = 0
+		roll     := uint16(generator.Intn(NumSides) + 1)
 		for {
-			sum += roll
-			rolls = append (rolls, roll)
-			if roll != NumSides {
+			res.Total += roll
+			res.List   = append (res.List, roll)
+			if roll != uint16(NumSides) {
 				break
 			}
-			roll = generator.Intn(NumSides) + 1
+			roll = uint16(generator.Intn(NumSides) + 1)
 		}
 		return
 	}	
 }
 
-// 
+// Rolls a die a set number of times. Returns as an array of DieRoll structs
+// Report without the explosion unless explosion array has two or more elements
+func RollDice (die func() *DieRoll, NumDice uint8) []DieRoll {
+	rolls := make([]DieRoll, NumDice)
+	for i := uint8(0); i < NumDice; i++ {
+		rolls[i] = *die()
+	}
+	return rolls
+}
 
 func main() {
-	roll := 0
-	list := []int{}
+	roll := new(DieRoll)
 	d4   := DieType( 4)
 	d6   := DieType( 6)
 	d8   := DieType( 8)
@@ -38,27 +54,29 @@ func main() {
 	d12  := DieType(12)
 	
 	for i := 0; i < 16; i++ {
-		roll, list = d4()
-		fmt.Println("Rolling 1d4: ", roll, list)
+		roll = d4()
+		fmt.Println("Rolling 1d4: ", roll.Total, roll.List)
 	}
 	fmt.Println()
 	for i := 0; i < 16; i++ {
-		roll, list = d6()
-		fmt.Println("Rolling 1d6: ", roll, list)
+		roll = d6()
+		fmt.Println("Rolling 1d6: ", roll.Total, roll.List)
 	}
 	fmt.Println()
 	for i := 0; i < 16; i++ {
-		roll, list = d8()
-		fmt.Println("Rolling 1d8: ", roll, list)
+		roll = d8()
+		fmt.Println("Rolling 1d8: ", roll.Total, roll.List)
 	}
 	fmt.Println()
 	for i := 0; i < 16; i++ {
-		roll, list = d10()
-		fmt.Println("Rolling 1d10: ", roll, list)
+		roll = d10()
+		fmt.Println("Rolling 1d10: ", roll.Total, roll.List)
 	}
 	fmt.Println()
 	for i := 0; i < 16; i++ {
-		roll, list = d12()
-		fmt.Println("Rolling 1d12: ", roll, list)
-	}
+		roll = d12()
+		fmt.Println("Rolling 1d12: ", roll.Total, roll.List)
+	} 
+	fmt.Println()
+	fmt.Println("4d12", RollDice(d12, 4))
 }
